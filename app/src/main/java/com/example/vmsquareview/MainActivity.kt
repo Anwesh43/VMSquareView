c7e02c6d60e892f499121f97edca964095a131c2
Anwesh43 <anweshthecool0@gmail.com>
2021-06-01 00:57:21 +0530

    adding fullScreen and view in activity

--- app/src/main/java/com/example/vmsquareview/MainActivity.kt
+++ app/src/main/java/com/example/vmsquareview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.vmsquareview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.linkedvmsquareview.LinkedVMSquareView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedVMSquareView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
