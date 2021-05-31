1cb92e3dbf461dd63c943a3e286410305d4dafa3
Anwesh43 <anweshthecool0@gmail.com>
2021-06-01 00:40:16 +0530

    adding LinkedVMSquareView class

--- linkedvmsquareview/src/main/java/com/example/linkedvmsquareview/LinkedVMSquareView.kt
+++ linkedvmsquareview/src/main/java/com/example/linkedvmsquareview/LinkedVMSquareView.kt
@@ -69,3 +69,19 @@ fun Canvas.drawVMSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawVMSquare(scale, w, h, paint)
 }
+
+class LinkedVMSquareView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
