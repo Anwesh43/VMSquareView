00d165688609ba9fa98f17065b925ae76f171d41
Anwesh43 <anweshthecool0@gmail.com>
2021-06-01 00:34:43 +0530

    adding Canvas extension methods

--- linkedvmsquareview/src/main/java/com/example/linkedvmsquareview/LinkedVMSquareView.kt
+++ linkedvmsquareview/src/main/java/com/example/linkedvmsquareview/LinkedVMSquareView.kt
@@ -30,3 +30,42 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawVMSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / lineFactor
+    val square : Float = Math.min(w, h) / squareFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val sf5 : Float = sf.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(180f * sf5)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        if (sf1 > 0) {
+            drawLine(0f, 0f, -size * sf1, size * sf1, paint)
+        }
+        if (sf2 > 0) {
+            drawLine(-size, size, -size - size * sf2, -size * sf2, paint)
+        }
+        save()
+        translate(-2 * size * sf4, 0f)
+        drawRect(RectF(-square / 2, -square * sf4, square / 2, 0f), paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawVMSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawVMSquare(scale, w, h, paint)
+}
