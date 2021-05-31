0160fc039e87d8371bda2a4452362be90ac8ded4
Anwesh43 <anweshthecool0@gmail.com>
2021-06-01 00:25:31 +0530

    adding constants and libraries

--- linkedvmsquareview/src/main/java/com/example/linkedvmsquareview/LinkedVMSquareView.kt
+++ linkedvmsquareview/src/main/java/com/example/linkedvmsquareview/LinkedVMSquareView.kt
@@ -1,2 +1,27 @@
 package com.example.linkedvmsquareview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#00C853",
+    "#304FFE",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val delay : Long = 20
+val scGap : Float = 0.02f / delay
+val squareFactor : Float = 14.9f
+val lineFactor : Float = 4.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val strokeFactor : Float = 90f
