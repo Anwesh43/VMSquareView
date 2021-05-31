5b3b34351a40433541190e1dd275564e363328a6
Anwesh43 <anweshthecool0@gmail.com>
2021-06-01 00:27:06 +0530

    adding Int and Float extension methods

--- linkedvmsquareview/src/main/java/com/example/linkedvmsquareview/LinkedVMSquareView.kt
+++ linkedvmsquareview/src/main/java/com/example/linkedvmsquareview/LinkedVMSquareView.kt
@@ -25,3 +25,8 @@ val squareFactor : Float = 14.9f
 val lineFactor : Float = 4.9f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val strokeFactor : Float = 90f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
