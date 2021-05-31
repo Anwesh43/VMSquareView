03831f7c666b7568b648a402bf97e4f664b2a288
Anwesh43 <anweshthecool0@gmail.com>
2021-06-01 00:46:01 +0530

    adding VMSNode class

--- linkedvmsquareview/src/main/java/com/example/linkedvmsquareview/LinkedVMSquareView.kt
+++ linkedvmsquareview/src/main/java/com/example/linkedvmsquareview/LinkedVMSquareView.kt
@@ -132,4 +132,45 @@ class LinkedVMSquareView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VMSNode(var i : Int, val state : State = State()) {
+
+        private var next : VMSNode? = null
+        private var prev : VMSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = VMSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawVMSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : VMSNode {
+            var curr : VMSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
